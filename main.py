4c46f47648042cf1ab13d46585cbfe634a55ed51
Kaia <kxena408@gmail.com>
2024-04-10 21:12:45 -0500

    Update main.py

--- main.py
+++ main.py
@@ -85,7 +85,7 @@ plot_raw_data()
 df_train = data[['Date','Close']]
 df_train = df_train.rename(columns={"Date": "ds", "Close": "y"})
 
-m = Prophet()
+m = Prophet() #yay
 m.fit(df_train)
 future = m.make_future_dataframe(periods=period)
 forecast = m.predict(future)
